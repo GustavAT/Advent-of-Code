62291cf5d15d4b579d33948a49ed4afffdfdfb9b
Andreas Tscheinig <andreas.tscheinig@dynatrace.com>
2020-12-03 08:24:03 +0100

    Implement day 3 challenge

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ Solutions to (all?) [Advent of Code 2020 <img src="https://www.pngrepo.com/png/3
 |-|-|-|
 | 1 | 🛠️ [Report Repair](https://adventofcode.com/2020/day/1) | [day01.ts](./src/day01.ts)|
 | 2 | 🔑 [Password Philosophy](https://adventofcode.com/2020/day/2) | [day02.ts](./src/day02.ts)|
+| 3 | 🛷 [Day 3: Toboggan Trajectory](https://adventofcode.com/2020/day/3) | [day03.ts](./src/day03.ts)|
 
 
 
---
+++ src/day03.ts
@@ -0,0 +1,36 @@
+import { readAllLines } from "./util";
+
+const trees = (input: string[], rowStep: number, columnStep: number): number => {
+    let trees = 0;
+
+    for (let row = 0, column = 0; row < input.length; row += rowStep, column += columnStep) {
+        const line = input[row];
+
+        // Repeat the pattern
+        if (column >= line.length) {
+            column = column - line.length;
+        }
+
+        if (line[column] === '#') {
+            trees++;
+        }
+    }
+
+    return trees;
+}
+
+const part1 = (input: string[]): number => {
+    return trees(input, 1, 3);
+}
+
+const part2 = (input: string[]): number => {
+    return trees(input, 1, 1) *
+        trees(input, 1, 3) *
+        trees(input, 1, 5) *
+        trees(input, 1, 7) *
+        trees(input, 2, 1);
+}
+
+const input = readAllLines('./res/input03.txt');
+console.log('Trees encountered (part 1):', part1(input));
+console.log('Trees encountered (part 2):', part2(input));
