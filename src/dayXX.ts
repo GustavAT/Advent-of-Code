72cc0e51d02a9cff9defcb4f335294e0d98c57cf
Andreas Tscheinig <andreas.tscheinig@dynatrace.com>
2021-12-04 08:44:18 +0100

    Add test input and solution templates

---
+++ src/dayXX.ts
@@ -0,0 +1,13 @@
+import { readAllLines } from './io.util';
+
+const part1 = (input: string[]): number => {
+  return -1;
+};
+
+const part2 = (input: string[]): number => {
+  return -1;
+};
+
+const input = readAllLines('./res/test_input.txt');
+console.log('part1:', part1(input));
+console.log('part2:', part2(input));
