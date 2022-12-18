f46f75b48902c1d317e4e7e467c8c152b18115a7
Andreas Tscheinig <andreas.tscheinig@dynatrace.com>
2022-12-18 17:02:31 +0100

    Add circular ring buffer

--- src/data-structures.ts
+++ src/data-structures.ts
@@ -1,9 +1,35 @@
 export class Node {
-    public value: number;
-    public previous!: Node;
-    public next!: Node;
+  public value: number;
+  public previous!: Node;
+  public next!: Node;
 
-    constructor (value: number) {
-        this.value = value;
+  constructor(value: number) {
+    this.value = value;
+  }
+}
+
+/**
+ * Circular ring buffer that gives the next element upon calling next().
+ */
+export class CircularArray<T> {
+  private index: number = 0;
+  private readonly source: T[];
+
+  constructor(source: T[]) {
+    this.source = source;
+  }
+
+  next(): T {
+    const element = this.source[this.index];
+    this.index++;
+    if (this.index === this.source.length) {
+      this.index = 0;
     }
+
+    return element;
+  }
+
+  getIndex(): number {
+    return this.index;
+  }
 }
